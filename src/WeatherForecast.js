167917ef069b4ce3fb26872e3f9b376b56029fdc
ayanna <ayannafofana.aaf@gmail.com>
2023-07-12 20:47:32 -0700

    reying to add react images at bottom

---
+++ src/WeatherForecast.js
@@ -0,0 +1,49 @@
+import React from "react";
+import WeatherIcon from "./weatherIcon";
+import "./WeatherForecast.css"
+
+export default function WeatherForecast(){
+return (
+  <div className="WeatherForecast">
+    <div className="row">
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+      <div className="col">
+        <div className="weatherForecast-day">Thu</div>
+        <WeatherIcon code="01d" size={36}/>
+        <span className="WeatherForecast-temperature-max">19°</span>
+        <span className="WeatherForecast-temperature-min">10°</span>
+      </div>
+  </div>
+  </div>
+);
+    
+}
