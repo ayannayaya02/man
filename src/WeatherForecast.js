8bf92ca92ad5f66427184862cb02b17729e8fb42
ayanna <ayannafofana.aaf@gmail.com>
2023-07-12 21:27:22 -0700

    coord

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,23 @@
 import React from "react";
+import axios from "axios";
 import WeatherIcon from "./weatherIcon";
 import "./WeatherForecast.css"
 
-export default function WeatherForecast(){
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-let apiKey="e2e5f0664e14425e87abaae94f48d5e8";
-let longitude=props.coordinates.lon;
-let latitude=props.coordinates.lat;
+export default function WeatherForecast(props){
+
+    function handleResponse(response) {
+      console.log(response.data);
+    }
+    console.log(props);
+
+    let apiKey = "e2e5f0664e14425e87abaae94f48d5e8";
+    let latitude = props.coordinates.lat;
+        let longitude = props.coordinates.lon;
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(handleResponse);
 
-function handleResponse(response){
-console.log(response.data);
-}
+
     
 return (
   <div className="WeatherForecast">
